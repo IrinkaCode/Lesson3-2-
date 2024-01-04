12a2619b54896f71021b3a05d755638baef04dfa
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-01-04 22:20:05 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿//Console.WriteLine("Добиться успеха легко,познать смысл - вот проблема");
+
+//string firstWord = "Добиться";
+//string secondWord = "успеха";
+//string thirdWord = "легко,";
+//string fourthWord = "познать";
+//string fifthWord = "смысл - ";
+//string sixthWord = "вот";
+//string seventhWord = "проблема";
+
+//string result = firstWord + " " + secondWord + " " + thirdWord + " " + fourthWord + " " + fifthWord + " " + sixthWord + " " + seventhWord;
+//Console.WriteLine(result);
+
+
+
+//string firstWord = "Добиться";
+//string secondWord = "успеха";
+//string thirdWord = "легко,";
+//string fourthWord = "познать";
+//string fifthWord = "смысл - ";
+//string sixthWord = "вот";
+//string seventhWord = "проблема";
+
+//string result = $"Давайте соединим строки {firstWord}  {secondWord}  {thirdWord}  {fourthWord}  {fifthWord}  {sixthWord}  {seventhWord} и получим {firstWord} {secondWord} {thirdWord} {fourthWord} {fifthWord} {sixthWord} {seventhWord}.";
+//Console.WriteLine(result);
+
+
+
+//string firstWord = "Добиться";
+//string secondWord = "успеха";
+//string thirdWord = "легко,";
+//string fourthWord = "познать";
+//string fifthWord = "смысл - ";
+//string sixthWord = "вот";
+//string seventhWord = "проблема";
+
+//string union = firstWord + " " + secondWord + " " + thirdWord + " " + fourthWord + " " + fifthWord + " " + sixthWord + " " + seventhWord;
+//string sentence = string.Concat(union, "!!!");
+//Console.WriteLine(sentence);
+
+
+//string firstWord = "Добиться";
+//string secondWord = "успеха";
+//string thirdWord = "легко,";
+//string fourthWord = "познать";
+//string fifthWord = "смысл - ";
+//string sixthWord = "вот";
+//string seventhWord = "проблема";
+
+//string union = firstWord + " " + secondWord + " " + thirdWord + " " + fourthWord + " " + fifthWord + " " + sixthWord + " " + seventhWord;
+//string sentence = string.Concat(union, "!!!");
+//string cutSentence = sentence.Substring(2);
+//Console.WriteLine(cutSentence);
+
+
+
+
+
+
+
+
+
+
